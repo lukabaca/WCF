7cc24bbb95c91ccaacdee5f13ae63951c4e4ca56
lukabaca <lkszblaszczyk@gmail.com>
2018-05-17 17:08:31 +0200

    dodano strone clienta

---
+++ Client/Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+using Client.ServiceReference;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                LibraryServiceClient client = new LibraryServiceClient();
+                Console.WriteLine(client.borrowBook(1).userID);
+
+                Console.WriteLine(client.getBook(1).title);
+
+                Console.WriteLine(client.getBook(-2).title);
+
+                Console.ReadLine();
+           
+
+            client.Close();
+        }
+    }
+}
